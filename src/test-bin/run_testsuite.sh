c5c48b906713202e5c1942d8c766a2f54340daae
Roland Olbricht <olbricht@wmaz9.math.uni-wuppertal.de>
2011-10-05 01:07:53 +0200

    Wokraround for the oversized meta test suite.

--- src/test-bin/run_testsuite.sh
+++ src/test-bin/run_testsuite.sh
@@ -16,16 +16,21 @@ DATA_SIZE="$1"
 BASEDIR="$(cd `dirname $0` && pwd)/.."
 NOTIMES="$2"
 
-$BASEDIR/test-bin/run_testsuite_template_db.sh $1 $2
+$BASEDIR/test-bin/run_testsuite_template_db.sh $DATA_SIZE $2
 
-$BASEDIR/test-bin/run_testsuite_osm_backend.sh $1 $2
+$BASEDIR/test-bin/run_testsuite_osm_backend.sh $DATA_SIZE $2
 
-$BASEDIR/test-bin/run_unittests_statements.sh $1 $2
+$BASEDIR/test-bin/run_unittests_statements.sh $DATA_SIZE $2
 
-$BASEDIR/test-bin/run_testsuite_osm3s_query.sh $1 $2
+$BASEDIR/test-bin/run_testsuite_osm3s_query.sh $DATA_SIZE $2
 
-$BASEDIR/test-bin/run_testsuite_diff_updater.sh $1 $2
+$BASEDIR/test-bin/run_testsuite_diff_updater.sh $DATA_SIZE $2
 
-$BASEDIR/test-bin/run_unittests_areas.sh $1 $2
+$BASEDIR/test-bin/run_unittests_areas.sh $DATA_SIZE $2
 
-$BASEDIR/test-bin/run_unittests_meta.sh $1 $2
+if [[ $DATA_SIZE -gt 800 ]]; then
+{
+  DATA_SIZE=800
+}; fi
+
+$BASEDIR/test-bin/run_unittests_meta.sh $DATA_SIZE $2
