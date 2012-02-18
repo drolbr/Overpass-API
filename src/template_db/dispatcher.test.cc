9f35b640cee8b8f7219ad041c4ae30a176669926
Roland Olbricht <olbricht@wmaz9.math.uni-wuppertal.de>
2012-02-19 00:11:23 +0100

    Started to tag all files with a license remark.

--- src/template_db/dispatcher.test.cc
+++ src/template_db/dispatcher.test.cc
@@ -1,3 +1,21 @@
+/** Copyright 2008, 2009, 2010, 2011, 2012 Roland Olbricht
+*
+* This file is part of Template_DB.
+*
+* Template_DB is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Template_DB is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "block_backend.h"
 #include "dispatcher.h"
 #include "file_blocks.h"
