b44fa3b5699512b3bb3b1fd5814bb03a812c2da3
Roland Olbricht <olbricht@wmaz9.math.uni-wuppertal.de>
2012-02-21 07:46:48 +0100

    Tagged the remaining files with license information.

--- src/expat/expat_justparse_interface.cc
+++ src/expat/expat_justparse_interface.cc
@@ -1,18 +1,22 @@
-/*****************************************************************
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the license contained in the
- * COPYING file that comes with the expat distribution.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * Must be used with Expat compiled for UTF-8 output.
- */
+/** Copyright 2008, 2009, 2010, 2011, 2012 Roland Olbricht
+*
+* This file is part of Overpass_API.
+*
+* Overpass_API is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Overpass_API is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Overpass_API.  If not, see <http://www.gnu.org/licenses/>.
+*
+* This file is derived from code shipped with the project Expat.
+*/
 
 #include <string>
 
--- src/pt_diagrams/read_input.h
+++ src/pt_diagrams/read_input.h
@@ -1,9 +1,23 @@
+/** Copyright 2008, 2009, 2010, 2011, 2012 Roland Olbricht
+*
+* This file is part of PT_Diagrams.
+*
+* PT_Diagrams is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* PT_Diagrams is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with PT_Diagrams.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef DE__OSM3S___PT_DIAGRAMS__READ_INPUT_H
 #define DE__OSM3S___PT_DIAGRAMS__READ_INPUT_H
-
-/*****************************************************************
- * Must be used with Expat compiled for UTF-8 output.
- */
 
 #include <map>
 #include <string>
