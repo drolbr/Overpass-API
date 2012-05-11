e0d31e83239b07d1db43105530450a234cce1837
Roland Olbricht <roland.olbricht@gmx.de>
2012-05-11 19:28:07 +0200

    Added the mechanism for a shared library.

---
+++ src/lib/overpass.cc
@@ -0,0 +1,9 @@
+#include "overpass.h"
+
+#include <iostream>
+
+using std::cout;
+
+void overpass_bbox()
+{
+}
---
+++ src/lib/overpass.h
@@ -0,0 +1,14 @@
+#ifndef DE__OVERPASS_API__LIBOVERPASS
+#define DE__OVERPASS_API__LIBOVERPASS
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void overpass_bbox();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
