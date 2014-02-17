2a6992b1adbeaced394ec4d35fe3e617bc248e76
Roland Olbricht <roland.olbricht@gmx.de>
2014-02-17 19:14:51 +0100

    make_area: fixed a bug.

--- src/overpass_api/statements/make_area.cc
+++ src/overpass_api/statements/make_area.cc
@@ -255,11 +255,11 @@ void Make_Area_Statement::execute(Resource_Manager& rman)
   set< Uint31_Index > coarse_indices;
   set< pair< Tag_Index_Local, Tag_Index_Local > > range_set;
   if (pivot_type == NODE)
-    coarse_indices.insert(mit->second.nodes.begin()->first.val() & 0xffffff00);
+    coarse_indices.insert(mit->second.nodes.begin()->first.val() & 0x7fffff00);
   else if (pivot_type == WAY)
-    coarse_indices.insert(mit->second.ways.begin()->first.val() & 0xffffff00);
+    coarse_indices.insert(mit->second.ways.begin()->first.val() & 0x7fffff00);
   else if (pivot_type == RELATION)
-    coarse_indices.insert(mit->second.relations.begin()->first.val() & 0xffffff00);
+    coarse_indices.insert(mit->second.relations.begin()->first.val() & 0x7fffff00);
   
   formulate_range_query(range_set, coarse_indices);
   
