4b89de99825cabadbd5a08f24a36bc843f696d9e
Roland Olbricht <roland.olbricht@gmx.de>
2014-10-29 07:33:46 +0100

    Incremented version number.

--- src/overpass_api/core/settings.cc
+++ src/overpass_api/core/settings.cc
@@ -90,7 +90,7 @@ Basic_Settings::Basic_Settings()
 
   base_directory("./"),
   logfile_name("transactions.log"),
-  shared_name_base("/osm3s_v0.7.50")
+  shared_name_base("/osm3s_v0.7.51")
 {}
 
 Basic_Settings& basic_settings()
