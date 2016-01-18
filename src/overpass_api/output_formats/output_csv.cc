ee5c37300ee27ac21eef9a2d74e317d2c5daa03f
Roland Olbricht <roland.olbricht@gmx.de>
2016-01-18 08:19:08 +0100

    Further implemented Output_CSV format.

--- src/overpass_api/output_formats/output_csv.cc
+++ src/overpass_api/output_formats/output_csv.cc
@@ -2,6 +2,28 @@
 #include "output_csv.h"
 
 
+namespace
+{
+  struct Object_Type
+  {
+    enum _ { invalid, node, way, relation, area };
+    
+    static std::string name(Object_Type::_ otype)
+    {
+      if (otype == Object_Type::node)
+        return "node";
+      else if (otype == Object_Type::way)
+        return "way";
+      else if (otype == Object_Type::relation)
+        return "relation";
+      else if (otype == Object_Type::area)
+        return "area";
+      return "unknown object";
+    }
+  };
+}
+
+
 bool Output_CSV::write_http_headers()
 {
   std::cout<<"Content-type: text/csv\n";
@@ -12,7 +34,17 @@ bool Output_CSV::write_http_headers()
 void Output_CSV::write_payload_header
     (const std::string& db_dir, const std::string& timestamp, const std::string& area_timestamp)
 {
-  // Intentionally empty
+  if (csv_settings.with_headerline)
+  {
+    for (std::vector< std::pair< std::string, bool > >::const_iterator it = csv_settings.keyfields.begin();
+        it != csv_settings.keyfields.end(); ++it)
+    {
+      std::cout<< (it->second ? "@" : "") << it->first;
+      if (it + 1 != csv_settings.keyfields.end())
+        std::cout<<csv_settings.separator;
+    }
+    std::cout<<'\n';
+  }
 }
 
 
@@ -34,6 +66,83 @@ void Output_CSV::display_error(const std::string& text)
 }
 
 
+template< typename Id_Type, typename OSM_Element_Metadata_Skeleton >
+void process_csv_line(Object_Type::_ otype, Id_Type id, const Opaque_Geometry& geometry,
+    const OSM_Element_Metadata_Skeleton* meta,
+    const std::vector< std::pair< std::string, std::string> >* tags,
+    const std::map< uint32, std::string >* users,
+    const Csv_Settings& csv_settings,
+    Output_Mode mode)
+{
+  std::vector< std::pair< std::string, bool > >::const_iterator it = csv_settings.keyfields.begin();
+  while (true)
+  {
+    if (!it->second)
+    {
+      if (tags)
+      {
+	for (std::vector< std::pair< std::string, std::string> >::const_iterator it_tags = tags->begin();
+	     it_tags != tags->end(); ++it_tags)
+	{
+	  if (it_tags->first == it->first)
+	  {
+	    std::cout<<it_tags->second;
+	    break;
+	  }
+	}
+      }
+    }
+    else
+    {
+      if (meta)
+      {
+        if (it->first == "version")
+          std::cout<<meta->version;
+        else if (it->first == "timestamp")
+          std::cout<<Timestamp(meta->timestamp).str();
+        else if (it->first == "changeset")
+          std::cout<<meta->changeset;
+        else if (it->first == "uid")
+          std::cout<<meta->user_id;
+        else if (it->first == "user")
+        {
+          std::map< uint32, std::string >::const_iterator uit = users->find(meta->user_id);
+          if (uit != users->end())
+            std::cout<<uit->second;
+        }
+      }
+
+      if (it->first == "id")
+      {
+        if (mode.mode & Output_Mode::ID)
+          std::cout<<id.val();
+      }
+      else if (it->first == "otype")
+        std::cout<<int(otype);
+      else if (it->first == "type")
+	std::cout<<Object_Type::name(otype);
+      else if (it->first == "lat")
+      {
+        if ((mode.mode & (Output_Mode::COORDS | Output_Mode::GEOMETRY | Output_Mode::BOUNDS | Output_Mode::CENTER))
+	    && geometry.has_center())
+          std::cout<<std::fixed<<std::setprecision(7)<<geometry.center_lat();
+      }
+      else if (it->first == "lon")
+      {
+        if ((mode.mode & (Output_Mode::COORDS | Output_Mode::GEOMETRY | Output_Mode::BOUNDS | Output_Mode::CENTER))
+	    && geometry.has_center())
+          std::cout<<std::fixed<<std::setprecision(7)<<geometry.center_lon();
+      }
+    }
+      
+    if (++it == csv_settings.keyfields.end())
+      break;
+    std::cout<<csv_settings.separator;
+  }
+  std::cout<<"\n";
+}
+
+
 void Output_CSV::print_item(const Node_Skeleton& skel,
       const Opaque_Geometry& geometry,
       const std::vector< std::pair< std::string, std::string > >* tags,
@@ -45,7 +154,7 @@ void Output_CSV::print_item(const Node_Skeleton& skel,
       const std::vector< std::pair< std::string, std::string > >* new_tags,
       const OSM_Element_Metadata_Skeleton< Node::Id_Type >* new_meta)
 {
-  //TODO
+  process_csv_line(Object_Type::node, skel.id, geometry, meta, tags, users, csv_settings, mode);
 }
 
 
@@ -61,7 +170,7 @@ void Output_CSV::print_item(const Way_Skeleton& skel,
       const std::vector< std::pair< std::string, std::string > >* new_tags,
       const OSM_Element_Metadata_Skeleton< Way::Id_Type >* new_meta)
 {
-  //TODO
+  process_csv_line(Object_Type::way, skel.id, geometry, meta, tags, users, csv_settings, mode);
 }
 
 
@@ -78,7 +187,7 @@ void Output_CSV::print_item(const Relation_Skeleton& skel,
       const std::vector< std::pair< std::string, std::string > >* new_tags,
       const OSM_Element_Metadata_Skeleton< Relation::Id_Type >* new_meta)
 {
-  //TODO
+  process_csv_line(Object_Type::relation, skel.id, geometry, meta, tags, users, csv_settings, mode);
 }
 
 
