2310fcec3c0b0038352bbfec6536d500618069d6
Roland Olbricht <roland.olbricht@gmx.de>
2018-01-21 09:20:57 +0100

    Implemented and tested center for derived geometry.

---
+++ src/overpass_api/statements/geometry_endomorphisms.cc
@@ -0,0 +1,42 @@
+/** Copyright 2008, 2009, 2010, 2011, 2012 Roland Olbricht
+*
+* This file is part of Overpass_API.
+*
+* Overpass_API is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* Overpass_API is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Overpass_API.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "../data/tag_store.h"
+#include "../data/utils.h"
+#include "geometry_endomorphisms.h"
+
+
+Geometry_Endom_Statement_Maker< Evaluator_Center > Evaluator_Center::statement_maker;
+Geometry_Endom_Evaluator_Maker< Evaluator_Center > Evaluator_Center::evaluator_maker;
+
+
+Opaque_Geometry* Evaluator_Center::process(Opaque_Geometry* geom) const
+{
+  if (!geom)
+    return 0;
+  
+  Opaque_Geometry* result = 0;
+  if (geom->has_center())
+    result = new Point_Geometry(geom->center_lat(), geom->center_lon());
+  else
+    result = new Null_Geometry();
+  
+  delete geom;
+  return result;
+}
---
+++ src/overpass_api/statements/geometry_endomorphisms.h
@@ -0,0 +1,134 @@
+/** Copyright 2008, 2009, 2010, 2011, 2012 Roland Olbricht
+*
+* This file is part of Overpass_API.
+*
+* Overpass_API is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* Overpass_API is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Overpass_API.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DE__OSM3S___OVERPASS_API__STATEMENTS__GEOMETRY_ENDOMORPHISMS_H
+#define DE__OSM3S___OVERPASS_API__STATEMENTS__GEOMETRY_ENDOMORPHISMS_H
+
+
+#include "../data/tag_store.h"
+#include "../data/utils.h"
+#include "evaluator.h"
+#include "statement.h"
+#include "unary_functions.h"
+
+#include <map>
+#include <string>
+
+
+/* == Geometry Endomorphisms ==
+
+Geometry endomorphisms are functions with one argument.
+Many of them help to normalize or check the value of its argument.
+They always first let their argument be evaluated.
+Their syntax is always
+
+  <Function Name>(<Evaluator>)
+*/
+
+template< typename Evaluator_ >
+struct Geometry_Endom_Statement_Maker : public Generic_Statement_Maker< Evaluator_ >
+{
+  Geometry_Endom_Statement_Maker() : Generic_Statement_Maker< Evaluator_ >(Evaluator_::stmt_name()) {}
+};
+
+
+template< typename Evaluator_ >
+struct Geometry_Endom_Evaluator_Maker : public Statement::Evaluator_Maker
+{
+  virtual Statement* create_evaluator(const Token_Node_Ptr& tree_it, Statement::QL_Context tree_context,
+      Statement::Factory& stmt_factory, Parsed_Query& global_settings, Error_Output* error_output)
+  {
+    if (!tree_it.assert_is_function(error_output) || !tree_it.assert_has_input_set(error_output, false)
+        || !tree_it.assert_has_arguments(error_output, true))
+      return 0;
+  
+    std::map< std::string, std::string > attributes;
+    Statement* result = new Evaluator_(tree_it->line_col.first, attributes, global_settings);
+    if (result)
+    {
+      Statement* rhs = stmt_factory.create_evaluator(tree_it.rhs(), tree_context, Statement::geometry);
+      if (rhs)
+        result->add_statement(rhs, "");
+      else if (error_output)
+        error_output->add_parse_error(Evaluator_::stmt_func_name() + "(...) needs an argument",
+            tree_it->line_col.first);
+    }
+    return result;
+  }
+
+  Geometry_Endom_Evaluator_Maker()
+  {
+    Statement::maker_by_func_name()[Evaluator_::stmt_func_name()].push_back(this);
+  }
+};
+
+
+template< typename Evaluator_ >
+struct Evaluator_Geometry_Endom_Syntax : public Evaluator_Geometry_Unary_Function
+{
+  Evaluator_Geometry_Endom_Syntax(int line_number_, const std::map< std::string, std::string >& input_attributes)
+    : Evaluator_Geometry_Unary_Function(line_number_)
+  {
+    std::map< std::string, std::string > attributes;
+    eval_attributes_array(Evaluator_::stmt_name(), attributes, input_attributes);
+  }
+
+  virtual std::string dump_xml(const std::string& indent) const
+  {
+    return indent + "<" + Evaluator_::stmt_name() + ">\n"
+        + (rhs ? rhs->dump_xml(indent + "  ") : "")
+        + indent + "</" + Evaluator_::stmt_name() + ">\n";
+  }
+
+  virtual std::string dump_compact_ql(const std::string&) const
+  {
+    return Evaluator_::stmt_func_name() + "(" + (rhs ? rhs->dump_compact_ql("") : "") + ")";
+  }
+
+  virtual std::string get_name() const { return Evaluator_::stmt_name(); }
+};
+
+
+/* === Center ===
+
+The function <em>center</em> returns the center of its argument.
+It expects a function that evaluates to a geometry.
+It then delivers the point that is at the center of the bounding box of the geometry.
+
+Its syntax is
+
+  center(<Evaluator>)
+
+*/
+
+class Evaluator_Center : public Evaluator_Geometry_Endom_Syntax< Evaluator_Center >
+{
+public:
+  static Geometry_Endom_Statement_Maker< Evaluator_Center > statement_maker;
+  static Geometry_Endom_Evaluator_Maker< Evaluator_Center > evaluator_maker;
+  static std::string stmt_func_name() { return "center"; }
+  static std::string stmt_name() { return "eval-center"; }
+
+  Evaluator_Center(int line_number_, const std::map< std::string, std::string >& input_attributes, Parsed_Query& global_settings)
+      : Evaluator_Geometry_Endom_Syntax< Evaluator_Center >(line_number_, input_attributes) {}
+
+  virtual Opaque_Geometry* process(Opaque_Geometry* geom) const;
+};
+
+
+#endif
