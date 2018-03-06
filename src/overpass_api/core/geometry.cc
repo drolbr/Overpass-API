4d962ce1f4431f0276fbeb6452ae025eba810f3c
Roland Olbricht <roland.olbricht@gmx.de>
2018-03-06 19:02:59 +0100

    Fixed a bug

--- src/overpass_api/core/geometry.cc
+++ src/overpass_api/core/geometry.cc
@@ -2185,6 +2185,7 @@ struct Spherical_Vector
   double y;
   double z;
   
+  double operator==(const Spherical_Vector& rhs) const { return x == rhs.x && y == rhs.y && z == rhs.z; }
   double operator*(const Spherical_Vector& rhs) const { return x*rhs.x + y*rhs.y + z*rhs.z; }
   
   static double deg_to_arc()
@@ -2225,10 +2226,15 @@ Proto_Hull::Proto_Hull(const Point_Double& s, const Point_Double& w, const Point
     segments.push_back(Hull_Segment(e));
   if (n != segments.back().ll_pt)
     segments.push_back(Hull_Segment(n));
+  if (segments.back().s_pt == segments.front().s_pt)
+    segments.pop_back();
   
   for (unsigned int i = 1; i < segments.size(); ++i)
     segments[i].edge = Spherical_Vector(segments[i-1].s_pt, segments[i].s_pt);
-  segments[0].edge = Spherical_Vector(segments.back().s_pt, segments.front().s_pt);
+  if (segments.size() > 1)
+    segments[0].edge = Spherical_Vector(segments.back().s_pt, segments.front().s_pt);
+  else
+    segments.clear();
 }
 
 
@@ -2240,7 +2246,7 @@ void Proto_Hull::enhance(const Point_Double& rhs)
   while (from_it != segments.end() && from_it->edge*s_pt < 1e-8)
     ++from_it;
   
-  if (from_it != segments.end())
+  if (from_it != segments.end() && from_it->edge*s_pt >= 1e-8)
   {
     std::vector< Hull_Segment >::iterator to_it = from_it;
     while (to_it != segments.end() && to_it->edge*s_pt >= 1e-8)
