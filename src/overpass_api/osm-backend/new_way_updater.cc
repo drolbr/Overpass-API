a8c0f165ed0c695e8985b52e8df37daa892577cb
Roland Olbricht <roland.olbricht@gmx.de>
2021-02-09 22:49:31 +0100

    Towards implementation of New_Way_Updater

--- src/overpass_api/osm-backend/new_way_updater.cc
+++ src/overpass_api/osm-backend/new_way_updater.cc
@@ -107,179 +107,15 @@ private:
 
 struct Changed_Objects_In_An_Idx
 {
-  std::vector< Way_Skeleton > current_to_del;
-  std::vector< Attic< Way_Skeleton > > attic_to_del;
-  std::vector< Attic< Way_Skeleton::Id_Type > > undeletes_to_del;
-  std::vector< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > current_meta_to_del;
-  std::vector< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > attic_meta_to_del;
+  std::vector< Way_Skeleton > existing_current;
+  std::vector< Attic< Way_Skeleton > > existing_attic;
+  std::vector< Attic< Way_Skeleton::Id_Type > > existing_undeletes;
+  std::vector< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > existing_current_meta;
+  std::vector< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > existing_attic_meta;
   std::vector< Way_Skeleton::Id_Type > attic_meta_covers_unchanged;
   std::vector< Way_Skeleton::Id_Type > current_meta_covers_unchanged;
   std::vector< Way_Skeleton::Id_Type > deleted_after_unchanged;
   Way_Event_Container events;
-};
-
-
-struct Way_Meta_Delta
-{
-  // Assertions: all sorted
-  // Ein Meta soll nachher exakt dann existieren, wenn
-  // - es bei einem nicht gelöschten Objekt vorkommt  oder
-  // - es echt früher als timestamp_per_id in seinem Index liegt (falsch)
-  // - es in deletions vorkommt und das letzte Meta vorher hier liegt (Problem!)
-  // Ok, wenn
-  // - nicht das erste Objekt dieser Id ~> Lücke zum vorherigen ist echte Lücke
-  // - meta.timestamp == obj.not_before
-  // Problem sonst, weil
-  // - existiert ein unverändertes Objekt mit Überlapp?
-  Way_Meta_Delta(...);
-
-  std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > >
-    current_to_add;
-  std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > >
-    current_to_delete;
-  std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > >
-    attic_to_add;
-  std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > >
-    attic_to_delete;
-
-// Strategie 1: nach Id trennen, ggf. per Pointer
-/* Für jede Id:
-    store_first = id gibt es in meta_covers_unchanged
-    while (attic)
-    {
-      while (new < attic)
-      {
-        attic_add.push(new)
-        last_before = advance(new, new.metatime)
-        add_deletion_if_applicable(..)
-      }
-      if (new == attic)
-      {
-        last_before = advance(new, attic.metatime)
-        add_deletion_if_applicable(..)
-      }
-      else if (!store_first)
-        attic_del.push(attic)
-
-      store_first = false;
-      ++attic;
-    }
-    if (current)
-    {
-      while (new < current)
-      {
-        attic_add.push(new)
-        last_before = advance(new, new.metatime)
-        add_deletion_if_applicable(..)
-      }
-      if (new == current)
-      {
-        last_before = advance(new, current.metatime)
-        if (last_before == NOW)
-          ++new;
-        else
-        {
-          move_to_attic.push(current)
-          add_deletion_if_applicable(..)
-        }
-      }
-      else if (store_first)
-        move_to_attic.push(current)
-      else
-        current_del.push(current)
-    }
-    while (new)
-    {
-      last_before = advance(new, new.metatime)
-      if (last_before < NOW)
-      {
-        attic_add.push(new)
-        add_deletion_if_applicable(..)
-      }
-      else
-        current_add.push(new)
-    }
-
-
-  uint64_t advance(new&, ref_time)
-  {
-    while (new.metatime == ref_time)
-    {
-      last_before = new.before
-      ++new;
-    }
-    return last_before
-  }
-
-  add_deletion_if_applicable(deletion&, attic&, id, last_before)
-  {
-    while (deletion < last_before)
-      ++deletion;
-    if (deletion == last_before)
-    {
-      if (attic == deletion)
-        ++attic
-      else
-        attic_add.push(deletion)
-      ++deletion;
-    }
-  }
-    */
-
-// Strategie 2: wie unten
-/*
-  {
-    for (i : changes_per_idx)
-    {
-      if (i.meta == i.next.meta && i.before == i.next.not_before)
-        continue;
-
-      while (attic.(id, timestamp) < i.meta.(id, timestamp))
-      {
-        while (attic_meta_covers_unchanged < attic.id)
-          ++attic_meta_covers_unchanged;
-        if (attic_meta_covers_unchanged == attic.id)
-          ++attic_meta_covers_unchanged;
-        else
-          attic_to_delete.push(attic)
-        ++attic
-      }
-      while (current.(id, timestamp) < i.meta.(id, timestamp))
-      {
-        while (current_meta_covers_unchanged < current.id)
-          ++current_meta_covers_unchanged;
-        if (current_meta_covers_unchanged == current.id)
-        {
-          attic_to_add.push(current);
-          ++current_meta_covers_unchanged;
-        }
-        current_to_delete.push(current)
-        ++current
-      }
-
-      if (i.before < NOW)
-      {
-        if (i.meta == attic.meta)
-          ++attic
-        else
-          attic_to_add.push(i)
-
-        if (attic_meta_covers_unchanged == id)
-          ++attic_meta_covers_unchanged;
-        add_or_keep_deletion_if_applicable(deletion, attic, id, i.before)
-      }
-      else
-      {
-        if (i.meta == current.meta)
-          ++current
-        else
-          current_to_add.push(i)
-
-        if (current_meta_covers_unchanged == id)
-          ++current_meta_covers_unchanged;
-      }
-    }
-  }*/
 };
 
 
@@ -527,6 +363,7 @@ void update_ways(Transaction& transaction, Data_From_Osc& new_data)
 //   dyn_perf.reset(new Perflog_Tree("first pass by idx"));
 
   std::map< Uint31_Index, Way_Event_Container > arrived_objects;
+  auto deletions;
 
   for (auto i_idx : pre_event_refs_by_idx)
   {
@@ -577,119 +414,72 @@ void update_ways(Transaction& transaction, Data_From_Osc& new_data)
 
 // resolve_coord_events(_Events_) -> { _Events_, map< Idx, _Events_ > arrived_objects }
     Way_Skeleton_Updater::resolve_coord_events(events_with_meta, changes.events, arrived_objects);
-    //TODO
   }
-// Nach Schleife verfügbar: { _Pre_Events_, map< Idx, { current_to_touch, attic_to_touch, undelete_to_touch, current_meta_to_touch, attic_meta_to_touch, _Events_ } >, arrived_objects }
 
 // join_arrived_objects(map< Idx, _Events_ >, map< Idx, {..., _Events_ } >&)
   join_arrived_objects(arrived_objects, changes_per_idx);
-// resolve_coord_events(_Pre_Events_, map< Idx, _Events_ >&)
-  auto deletions;
-  Way_Skeleton_Updater::resolve_coord_events(moved_coords, pre_events, changes_per_idx, deletions);
 
-  Way_Meta_Delta meta_delta(
-      changes_per_idx, deletions, timestamps_per_id);
+  Way_Skeleton_Updater::resolve_coord_events(pre_events, moved_coords, changes_per_idx, deletions);
+  std::sort(deletions.begin(), deletions.end());
 
-// Jetzt: map< Idx, {..., _Events_ } > enthält die vollständige zukünftige Struktur
+  //TODO: idx_mapfile
+  //mapfile_io.compute_and_write_idx_lists(nodes_meta_to_move_to_attic, nodes_meta_to_add, nodes_attic_meta_to_add);
+  {
+    std::map< Uint31_Index, std::vector< Way_Skeleton > > current_to_delete;
+    std::map< Uint31_Index, std::vector< Way_Skeleton > > current_to_add;
+    std::map< Uint31_Index, std::vector< Attic< Way_Skeleton > > > attic_to_delete;
+    std::map< Uint31_Index, std::vector< Attic< Way_Skeleton > > > attic_to_add;
 
-// vor update_meta: steht als Struktur für alle Idxe fertig bereit
-// - Sonderregel zum Meta für schlussendlich gelöschtes Objekt
-// current_meta gehört hierher genau dann, wenn letztes Way_Skeleton nicht null ist
-// - zusätzlich current_meta_to_delete_benötigt, kann Idx-lokal aus Bestand berechnet werden
-// alle attic_meta werden dedupliziert und nur dann nicht (erneut) angelegt, wenn sie sich gegen attic_meta_to_delete aufheben
-// - zusätzlich attic_meta_to_delete_benötigt, kann Idx-lokal aus Bestand berechnet werden
-// current gehört hierher genau dann, wenn letztes Way_Skeleton nicht null ist
-// - zusätzlich current_to_delete (für Gleichheit wohl nur Id) benötigt, kann Idx-lokal aus Bestand berechnet werden
-// alle attic werden dedupliziert und nur dann nicht (erneut) angelegt, wenn sie sich gegen attic_to_delete aufheben
-// - zusätzlich attic_to_delete benötigt, kann Idx-lokal aus Bestand berechnet werden
-// alle undelete werden dedupliziert und nur dann nicht (erneut) angelegt, wenn sie sich gegen undelete_to_delete aufheben
-// - zusätzlich undelete_to_delete benötigt, kann Idx-lokal aus Bestand berechnet werden
+    for (const auto& i : changes_per_idx)
+    {
+      Way_Skeleton_Delta skel_delta(
+          i.events, i.existing_current, i.existing_attic);
+      current_to_delete[i.idx].swap(skel_delta.current_to_delete);
+      attic_to_delete[i.idx].swap(skel_delta.attic_to_delete);
+      current_to_add[i.idx].swap(skel_delta.current_to_add);
+      attic_to_add[i.idx].swap(skel_delta.attic_to_add);
+    }
 
+    update_elements(current_to_delete, current_to_add, transaction, *osm_base_settings().WAYS);
+    update_elements(attic_to_delete, attic_to_add, transaction, *attic_settings().WAYS);
+  }
+  {
+    std::map< Uint31_Index, std::vector< Attic< Way_Skeleton::Id_Type > > > undeletes_to_delete;
+    std::map< Uint31_Index, std::vector< Attic< Way_Skeleton::Id_Type > > > undeletes_to_add;
 
-  // Ansatz: aus pre_events die pre_events_per_computed_idx berechnen.
-  // Sind per Idx ein sortierter Container von { Elem, Meta, timestamp_end },
-  // wobei nur in wenigen Fällen mehr als ein Objekt entstehen wird.
+    for (const auto& i : changes_per_idx)
+    {
+      Way_Undelete_Delta undel_delta(
+          i.events, i.existing_current, i.deleted_after_unchanged);
+      undeletes_to_delete[i.idx].swap(undel_delta.undeletes_to_delete);
+      undeletes_to_add[i.idx].swap(undel_delta.undeletes_to_add);
+    }
 
-  // Mechanismus für Meta:
-  // - Pre_Events hat Vorrang, kann aber Löcher haben (selten)
-  // - Pre_Events bringen ihr Meta mit
-  // - implicit_events als zweites, aber ggf. von Undelete betroffene Abschnitte (andere Idxe!) ignorieren
-  // - current und attic nachrangig, lösen keine Veränderung aus
-  // - dupliziertes Attic ist kein Problem, da vom Update-Meachnismus bereinigt
+    update_elements(undeletes_to_delete, undeletes_to_add, transaction, *attic_settings().WAYS_UNDELETED);
+  }
+  {
+    std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > >
+        current_meta_to_delete;
+    std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > >
+        attic_meta_to_delete;
+    std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > >
+        current_meta_to_add;
+    std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Way_Skeleton::Id_Type > > >
+        attic_meta_to_add;
 
-  // Idx-Sichten?
-  // - für pre_events nach der Schleife möglich (wegen Zeitbegrenzung)
-  // - für implicit_pre_events ab Einkürzen möglich (bereits am Ende des Schleifenrumpfs)
-  // - beides notwendig für Meta-Zuordnung
+    for (const auto& i : changes_per_idx)
+    {
+      Way_Meta_Delta meta_delta(
+          i.events, i.existing_current_meta, i.existing_attic_meta,
+          deletions,
+          i.current_meta_covers_unchanged, i.attic_meta_covers_unchanged);
+      current_meta_to_delete[i.idx].swap(meta_delta.current_to_delete);
+      attic_meta_to_delete[i.idx].swap(meta_delta.attic_to_delete);
+      current_meta_to_add[i.idx].swap(meta_delta.current_to_add);
+      attic_meta_to_add[i.idx].swap(meta_delta.attic_to_add);
+    }
 
-//   std::cerr<<"A "<<skels_per_idx.size()<<' '<<coord_dates.size()<<'\n';
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("nodes_meta_to_add ff"));
-// 
-  std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Node_Skeleton::Id_Type > > >
-      nodes_meta_to_add;
-  std::map< Uint31_Index, std::set< OSM_Element_Metadata_Skeleton< Node_Skeleton::Id_Type > > >
-      nodes_attic_meta_to_add;
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("Node_Meta_Updater::create_update_for_nodes_meta"));
-      // templatisieren!
-      // deduplizieren
-  Meta_Updater::create_update_for_nodes_meta(
-      pre_events, nodes_meta_to_move_to_attic, nodes_meta_to_add, nodes_attic_meta_to_add);
-  // ways_meta_to_add += meta of pre_events_per_computed_idx with timestamp_end == NOW ...
-  // ways_meta_to_delete += ways_to_move
-  // attic_ways_meta_to_add += ways_to_move + meta of pre_events_per_computed_idx with timestamp_end != NOW
-  // attic_ways_meta_to_delete already complete
-
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("mapfile_io.compute_and_write_idx_lists"));
-  // compute nodes_map, nodes_attic_map and nodes_idx_lists from pre_event_refs_by_idx, nodes_meta_to_add, and nodes_attic_meta_to_add
-  // write meta, nodes_map, nodes_attic_map, nodes_idx_lists
-  mapfile_io.compute_and_write_idx_lists(nodes_meta_to_move_to_attic, nodes_meta_to_add, nodes_attic_meta_to_add);
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("update_elements(meta)"));
-  update_elements(nodes_meta_to_move_to_attic, nodes_meta_to_add, transaction, *meta_settings().NODES_META);
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("update_elements(attic_meta)"));
-  update_elements(
-      decltype(nodes_attic_meta_to_add)(), nodes_attic_meta_to_add, transaction, *attic_settings().NODES_META);
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("nodes_to_delete ff"));
-// 
-//   std::map< Uint31_Index, std::set< Node_Skeleton > > nodes_to_delete;
-//   std::map< Uint31_Index, std::set< Node_Skeleton > > nodes_to_add;
-//   std::map< Uint31_Index, std::set< Attic< Node_Skeleton > > > nodes_attic_to_delete;
-//   std::map< Uint31_Index, std::set< Attic< Node_Skeleton > > > nodes_attic_to_add;
-//   std::map< Uint31_Index, std::set< Attic< Node_Skeleton::Id_Type > > > nodes_undelete_to_delete;
-//   std::map< Uint31_Index, std::set< Attic< Node_Skeleton::Id_Type > > > nodes_undelete_to_add;
-// 
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("second pass by idx"));
-//   for (auto i_idx : pre_event_refs_by_idx)
-//   {
-//     Uint31_Index working_idx = i_idx.first;
-//     Node_Skeletons_Per_Idx& skels = skels_per_idx[working_idx];
-// 
-//     Node_Event_List events(working_idx, skels, i_idx.second, pre_events);
-//     Prepare_Node_Update::create_update_for_nodes(
-//         events, nodes_to_delete[working_idx], nodes_to_add[working_idx]);
-//     Prepare_Node_Update::create_update_for_nodes_attic(
-//         events, nodes_attic_to_delete[working_idx], nodes_attic_to_add[working_idx]);
-//     Prepare_Node_Update::create_update_for_nodes_undelete(
-//         events, nodes_undelete_to_delete[working_idx], nodes_undelete_to_add[working_idx]);
-//   }
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("update_elements(nodes)"));
-// 
-//   // write nodes, nodes_attic, nodes_undelete
-//   update_elements(nodes_to_delete, nodes_to_add, transaction, *osm_base_settings().NODES);
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("update_elements(nodes_attic)"));
-//   update_elements(nodes_attic_to_delete, nodes_attic_to_add, transaction, *attic_settings().NODES);
-//   dyn_perf.reset(0);
-//   dyn_perf.reset(new Perflog_Tree("update_elements(nodes_undeleted)"));
-//   update_elements(
-//       nodes_undelete_to_delete, nodes_undelete_to_add, transaction, *attic_settings().NODES_UNDELETED);
-
-  //TODO: tags, nodes_for_ways
+    update_elements(current_meta_to_delete, current_meta_to_add, transaction, *meta_settings().WAYS_META);
+    update_elements(attic_meta_to_delete, attic_meta_to_add, transaction, *attic_settings().WAYS_META);
+  }
 }
