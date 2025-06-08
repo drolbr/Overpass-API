9c344122afb69136b821de11d1ea04f3fe2e8905
Roland Olbricht <roland.olbricht@gmx.de>
2025-06-09 00:35:39 +0200

    Automated commit for release 0.7.62.6

--- src/overpass_api/core/settings.cc
+++ src/overpass_api/core/settings.cc
@@ -102,8 +102,8 @@ Basic_Settings::Basic_Settings()
   db_logfile_name("database.log"),
   client_logfile_name("transactions.log"),
   shared_name_base("/osm3s"),
-  version("0.7.62.5"),
-  source_hash("1bd436f1a1aee62297918f468225a076be7046a8"),
+  version("0.7.62.6"),
+  source_hash("621661ead987163e2bacea1c85ea75e301b2b1d1"),
 #ifdef HAVE_LZ4
   compression_method(File_Blocks_Index_Base::LZ4_COMPRESSION),
 #else
